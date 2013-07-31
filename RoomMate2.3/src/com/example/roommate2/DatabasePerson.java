eb2b25351b7309245dd5301c600df67260c3cd87
siyaohan <hansiyao2@gmail.com>
2013-07-31 20:36:16 +0800

    Add DatabasePerson

---
+++ RoomMate2.3/src/com/example/roommate2/DatabasePerson.java
@@ -0,0 +1,17 @@
+package com.example.roommate2;
+
+public class DatabasePerson {
+
+	public DatabasePerson() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
